88c815d6582f46bbd68eb1d442cf4b9546d6f7c8
Wilson-Rodriguez <wilsonalex2002@gmail.com>
2025-08-31 17:00:07 -0600

    Add div creation for the sketch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hola")
+const gridSize = document.querySelector("#gridSize")
+const gridContainer = document.querySelector("#gridContainer")
+
+gridSize.addEventListener("click", () => {
+    const divSize = prompt("Enter the size for the grid")
+    createGrid(Number(divSize))
+})
+
+function createGrid(divSize) {
+    const error = "Input must be a valid whole number between 1-100"
+
+    if (isNaN(divSize) || !Number.isInteger(divSize)) {
+        alert(error)
+        return 
+    }
+
+    const divWidthHeight = `${800/divSize}px`
+
+    for (let i=0; i<divSize*divSize; i++) {
+        let element = document.createElement("div")
+        element.style.width = divWidthHeight
+        element.style.height = divWidthHeight
+        element.style.backgroundColor = "blue"
+        gridContainer.appendChild(element)
+    }
+
+}
